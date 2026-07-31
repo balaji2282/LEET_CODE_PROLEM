0fff81dd8d3a72bbd0e55ca03774fbd5f6eb390c
Balaji.B <balajibaskaran01@gmail.com>
2026-07-31 13:40:36 +0530

    Time: 6434 ms (7.75%) | Memory: 19.3 MB (89.79%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        if not p:
+            return not s
+
+        first = bool(s) and (s[0] == p[0] or p[0] == ".")
+
+        if len(p) >= 2 and p[1] == "*":
+            return self.isMatch(s, p[2:]) or (first and self.isMatch(s[1:], p))
+        else:
+            return first and self.isMatch(s[1:], p[1:])
